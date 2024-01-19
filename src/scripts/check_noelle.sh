219b5be8187c00b6f5d42ac2f571a4830096de3a
Simone Campanoni <simo.xan@gmail.com>
2024-01-19 16:40:36 -0600

    [scripts] use the NOELLE available in the environment

--- src/scripts/check_noelle.sh
+++ src/scripts/check_noelle.sh
@@ -5,3 +5,12 @@ if ! command -v noelle-config &> /dev/null ; then
   echo "Please install NOELLE before compiling GINO";
   exit 1;
 fi
+
+# Fetch the root of the current GINO git repository
+rootGitRepo="`git rev-parse --show-toplevel`" ;
+
+# Fetch the installation directory of NOELLE that is used in the environment
+noelleInstallDir="`noelle-config --prefix`";
+
+# Specify to CMake which NOELLE to use for compiling GINO
+echo "include_directories(\"${noelleInstallDir}/include\")" >> ${rootGitRepo}/src/scripts/DependencesCMake.txt ;
